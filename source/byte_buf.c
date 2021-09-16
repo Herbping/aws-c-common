615d7118509b3f8aa80f9bfc84bf9188886a0d3d
Qinheping Hu <qinhh@amazon.com>
2021-09-16 16:59:34 -0500

    memcmp

--- source/byte_buf.c
+++ source/byte_buf.c
@@ -426,6 +426,41 @@ bool aws_array_eq_ignore_case(
     return true;
 }
 
+int _memcmp(const void *s1, const void *s2, size_t n)
+{
+  __CPROVER_HIDE:;
+  int res=0;
+  #ifdef __CPROVER_STRING_ABSTRACTION
+  __CPROVER_precondition(__CPROVER_buffer_size(s1)>=n,
+                         "memcmp buffer overflow of 1st argument");
+  __CPROVER_precondition(__CPROVER_buffer_size(s2)>=n,
+                         "memcmp buffer overflow of 2nd argument");
+  #else
+  __CPROVER_precondition(__CPROVER_r_ok(s1, n),
+                         "memcmp region 1 readable");
+  __CPROVER_precondition(__CPROVER_r_ok(s2, n),
+                         "memcpy region 2 readable");
+
+  const unsigned char *sc1=s1, *sc2=s2;
+  for(; n!=0; n--)
+  __CPROVER_loop_invariant (
+    sc1 != NULL && sc2 != NULL && __CPROVER_IS_INVALID_POINTER(sc1) && __CPROVER_IS_INVALID_POINTER(sc2)
+    (0 <= __CPROVER_POINTER_OFFSET(sc1)) && (__CPROVER_POINTER_OFFSET(sc1) < __CPROVER_OBJECT_SIZE(sc1)) && 
+    (0 <= __CPROVER_POINTER_OFFSET(sc2)) && (__CPROVER_POINTER_OFFSET(sc2) < __CPROVER_OBJECT_SIZE(sc2)) &&
+    __CPROVER_same_object(sc1, s1) && __CPROVER_same_object(sc2, s2) && 
+    (__CPROVER_POINTER_OFFSET(sc1) == __CPROVER_POINTER_OFFSET(sc2)) &&
+    (n + __CPROVER_POINTER_OFFSET(sc1) < __CPROVER_OBJECT_SIZE(sc1)) &&
+    (n + __CPROVER_POINTER_OFFSET(sc2) < __CPROVER_OBJECT_SIZE(sc2)) 
+  )
+  {
+    res = (*sc1++) - (*sc2++);
+    if (res != 0)
+      return res;
+  }
+  #endif
+  return res;
+}
+
 bool aws_array_eq(const void *const array_a, const size_t len_a, const void *const array_b, const size_t len_b) {
     AWS_PRECONDITION(
         (len_a == 0) || AWS_MEM_IS_READABLE(array_a, len_a), "Input array [array_a] must be readable up to [len_a].");
@@ -440,7 +475,7 @@ bool aws_array_eq(const void *const array_a, const size_t len_a, const void *con
         return true;
     }
 
-    return !memcmp(array_a, array_b, len_a);
+    return !_memcmp(array_a, array_b, len_a);
 }
 
 bool aws_array_eq_c_str_ignore_case(const void *const array, const size_t array_len, const char *const c_str) {
